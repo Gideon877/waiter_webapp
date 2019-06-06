b31adf269bd7aaf1da2ee5d474a7527ea418a09c
Thabang Gideon Magaola <gideon877@live.com>
2019-06-06 10:39:16 +0200

    changes

---
+++ index.sh
@@ -0,0 +1,8 @@
+yarn add body-parser;
+yarn add express;
+yarn add express-flash;
+yarn add express-handlebars;
+yarn add express-session;
+yarn add mongoose;
+yarn add nodemon;
+yarn add semantic-ui;
